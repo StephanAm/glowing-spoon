1e5de4d1dc1fa775b6daf4003b6e8f3383881c51
Stephan Marais <stephan.marais@entelect.co.za>
2017-10-04 07:34:55 +0200

    Add react router

---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+class HomePage extends React.Component{
+    render(){
+        return(
+        <div>
+            <h1>About</h1>
+        </div>
+        );
+    }
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router,browserHistory} from 'react-router';
+import routes from './routes';
+
+ReactDOM.render(
+    <Router routes={routes} history={browserHistory}/>,
+    document.getElementById('root')
+);
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import {Router,browserHistory} from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+*/
